0ce9428d997c0ecd82898421e18b591a68d5c95e
mahadee hasan abir <37393027+mahade10@users.noreply.github.com>
2022-03-07 00:13:56 +0600

    Update problem12VGG16.py

--- problem12VGG16.py
+++ problem12VGG16.py
@@ -1,9 +1,8 @@
-from tensorflow.keras.layers import Dense, Input, Flatten, Conv2D, MaxPool2D
+from tensorflow.keras.layers import Dense, Input, Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
-from keras.preprocessing.image import img_to_array, array_to_img
 import numpy as np
 
 IMG_W = 32
@@ -35,28 +34,27 @@ def prepare_data():
     (trainX, trainY), (testX, testY) = mnist.load_data()
     print(trainX.shape, trainY.shape, testX.shape, testY.shape)
 
-    index = np.argwhere(trainY < 2)
-    trainX = trainX[index]
-    trainY = trainY[index]
+    train_indices = np.argwhere(trainY < 2)
+    test_indices = np.argwhere(testY < 2)
+    train_indices = np.squeeze(train_indices)
+    test_indices = np.squeeze(test_indices)
 
-    index = np.argwhere(testY < 2)
-    testX = testX[index]
-    testY = testY[index]
+    trainX = trainX[train_indices]
+    trainY = trainY[train_indices]
+    testX = testX[test_indices]
+    testY = testY[test_indices]
 
-    trainX = np.dstack([trainX] * 3)
-    testX = np.dstack([testX] * 3)
+    trainX = np.pad(trainX, ((0, 0), (2, 2), (2, 2)), 'constant')
+    testX = np.pad(testX, ((0, 0), (2, 2), (2, 2)), 'constant')
 
-    trainX = trainX.reshape(-1, 28, 28, 3)
-    testX = testX.reshape(-1, 28, 28, 3)
-
-    trainX = np.asarray([img_to_array(array_to_img(im, scale=False).resize((IMG_W, IMG_H))) for im in trainX])
-    testX = np.asarray([img_to_array(array_to_img(im, scale=False).resize((IMG_W, IMG_H))) for im in testX])
+    trainX = np.stack((trainX,) * 3, axis=-1)
+    testX = np.stack((testX,) * 3, axis=-1)
 
     classN = 2
     trainY = to_categorical(trainY, classN)
     testY = to_categorical(testY, classN)
 
-    # To convert pixel values from 0-255 into 0-1.
+    # To convert pixel values from 0-255 into 0-1.    
     trainX = trainX.astype(np.float32)
     testX = testX.astype(np.float32)
     trainX /= 255
@@ -74,7 +72,7 @@ def main():
     model = build_model()
 
     model.compile(loss='mse', optimizer='rmsprop')
-    model.fit(trainX, trainY, epochs=10, batch_size=16, validation_split=0.2)
+    model.fit(trainX, trainY, epochs=50, batch_size=32, validation_split=0.2)
     model.compile(metrics='accuracy')
     model.evaluate(testX, testY)
 
